80b1dfd8d80ec8677155054f9963693643ff5099
HungLeManh <113190663+HungLeManh@users.noreply.github.com>
2022-11-07 19:38:41 +0700

    commit 1

--- src/uet/oop/bomberman/entities/characters/enemies/Balloon.java
+++ src/uet/oop/bomberman/entities/characters/enemies/Balloon.java
@@ -1,14 +1,30 @@
 package uet.oop.bomberman.entities.characters.enemies;
 
 import javafx.scene.image.Image;
+import uet.oop.bomberman.entities.characters.enemies.ai.LowAI;
+import uet.oop.bomberman.graphics.Sprite;
 
 public class Balloon extends Enemy {
     public Balloon(int x, int y, Image img) {
-        super( x, y, img);
+        super(x, y, img);
+        spriteMove = new Sprite[]{
+                Sprite.balloon_left1, Sprite.balloon_left2, Sprite.balloon_left3,
+                Sprite.balloon_right1, Sprite.balloon_right2, Sprite.balloon_right3,
+                Sprite.balloon_left1, Sprite.balloon_left2, Sprite.balloon_left3,
+                Sprite.balloon_right1, Sprite.balloon_right2, Sprite.balloon_right3,
+        };
+        speed = 1;
     }
 
     @Override
     public void update() {
-        super.update();
+        LowAI lowAI = new LowAI();
+        direction = lowAI.calculateDirection();
+        int d = direction;
+        if(canMove(getXUnit(), getYUnit())){
+            for(int i=0; i< 10; i++){
+                move(32 - speed, 32, spriteMove);
+            }
+        }
     }
 }
